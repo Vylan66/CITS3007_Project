4ccea78477170234df398d56b6134ab5e6739d36
Divrr <96827300+Divrr@users.noreply.github.com>
2026-04-21 06:26:56 +0000

    implement reading data with no compression

--- bun_parse.c
+++ bun_parse.c
@@ -1,7 +1,10 @@
+#define _LARGEFILE64_SOURCE // TODO: check if this is entirely necessary. See: man fseeko, man lseek
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <sys/types.h>
 
 #include "bun.h"
 
@@ -14,6 +17,48 @@ static u32 read_u32_le(const u8 *buf, size_t offset) {
      | (u32)buf[offset + 1] << 8
      | (u32)buf[offset + 2] << 16
      | (u32)buf[offset + 3] << 24;
+}
+
+bun_result_t bun_read_data(BunHeader *header, BunParseContext *ctx, BunAssetRecord *entry, FILE *out_fptr) {
+  // Cases: data section out of bounds, asset record start out of bounds, 
+  // asset record end out of bounds, compression unsupported
+  if (header->data_section_offset >= (uint64_t)ctx->file_size) {return BUN_MALFORMED;}
+  if (entry->data_offset > (uint64_t)ctx->file_size - header->data_section_offset) {return BUN_MALFORMED;} 
+
+  uint64_t actual_offset = header->data_section_offset + entry->data_offset;
+
+  if (entry->data_size > (uint64_t)ctx->file_size - actual_offset) {return BUN_MALFORMED;}
+  if (entry->compression > 1) {return BUN_UNSUPPORTED;}
+
+  if (fseeko(ctx->file, (off_t)actual_offset, SEEK_SET) != 0) {
+    return BUN_ERR_IO;
+  }
+  uint64_t remaining = entry->data_size;
+  uint8_t buf[64*1024];
+    
+  switch (entry->compression) {
+  case 0: /* raw copy */
+      while (remaining > 0) {
+          size_t toread = (size_t)(remaining < sizeof(buf) ? remaining : sizeof(buf));
+          size_t n = fread(buf, 1, toread, ctx->file);
+          if (n == 0) {return BUN_MALFORMED;}
+          if (fwrite(buf, 1, n, out_fptr) != n) {return BUN_MALFORMED;}
+          remaining -= n;
+      }
+      break;
+  case 1: /* RLE decode */
+    printf("RLE SECTION IS NOT DONE YET. BE WITH YOU SOON!");
+    return BUN_UNSUPPORTED;
+    break;
+  case 2: /* zlib decode */
+    printf("ZLIB MAY NOT BE SUPPORTED!");
+    return BUN_UNSUPPORTED;
+    break;
+  default:
+    return BUN_UNSUPPORTED;
+  }
+
+  return BUN_OK;
 }
 
 //
